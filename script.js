8be80724258e8d3de47cf44e9acf9569abdeef4b
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-01 12:01:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+// script.js
+window.onload = function () {
+    let element = document.getElementById('level');
+    let level = 1;
+    while (element.parentElement) {
+        level++;
+        element = element.parentElement;
+    }
+    alert(`The level of the element is: ${level}`);
+};
